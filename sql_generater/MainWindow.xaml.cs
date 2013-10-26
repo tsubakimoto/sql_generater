554d9a9791d48a36848aed5500538d527d657a13
Dev Tsubaki <dev.tsubaki@gmail.com>
2013-10-26 10:25:36 +0900

    no message

--- sql_generater/MainWindow.xaml.cs
+++ sql_generater/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -11,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace sql_generater
 {
@@ -19,9 +21,55 @@ namespace sql_generater
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string prevDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        public MyProperty Pt { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = Pt;
+        }
+
+        private void BtnDialog_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog()
+            {
+                FileName = string.Empty,
+                DefaultExt = "*.log",
+                InitialDirectory = prevDirectory
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                var fi = new FileInfo(dialog.FileName);
+                if (fi.Exists)
+                {
+                    prevDirectory = fi.DirectoryName;
+                    this.LogFile.Text = fi.FullName;
+
+                    ShowLog(fi);
+                }
+                else
+                {
+                    this.ProcessMsg.Text = "ファイルが見つかりません。";
+                    this.ProcessMsg.Foreground = new SolidColorBrush(Colors.Red);
+                }
+            }
+        }
+
+        private void BtnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowLog(FileInfo fi)
+        {
+            using (var st = fi.OpenText())
+            {
+                this.AnalyzedLog.Text = st.ReadToEnd();
+            }
         }
     }
 }
---
+++ sql_generater/MyProperty.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace sql_generater
+{
+    public class MyProperty : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+#if false
+        private string _Message = string.Empty;
+        public string Message
+        {
+            get { return this._Message; }
+            set
+            {
+                if (value != this._Message)
+                {
+                    this._Message = value;
+                    NotifyPropertyChanged("Message");
+                }
+            }
+        }
+#endif
+    }
+}
